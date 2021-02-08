0deb38a3bec1edd5db530858aa1682627cbc5458
szattila98 <clownface68@gmail.com>
2021-02-08 10:11:03 +0100

    Backend project structure and basic dependencies added.

---
+++ backend/src/main/java/hu/miskolc/uni/robosoccer/RobosoccerApplication.java
@@ -0,0 +1,13 @@
+package hu.miskolc.uni.robosoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobosoccerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobosoccerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/miskolc/uni/robosoccer/RobosoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.miskolc.uni.robosoccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobosoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
